4aa17e660d62564af5ffd5047bc572ae5d662b10
Agathe Battestini <agathe@storenvy.com>
2014-03-22 10:29:13 -0700

    added credentials for foursquare and instagram. things seem to work

--- IOSLiiffee/IOSLiiffee/AppDelegate.m
+++ IOSLiiffee/IOSLiiffee/AppDelegate.m
@@ -12,6 +12,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [self setupFoursquare];
+    [self setupInstagram];
+
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
@@ -44,6 +47,37 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - 3rd Party apps
+
+- (void)setupFoursquare
+{
+    [Foursquare2 setupFoursquareWithClientId:@"HKYQG3UVFUV51A0STFEXL3ANNALDIVCZOYJVK14AFYA5IFQ1"
+                                      secret:@"REK5RKPZG3JAOIWC0Q1KXKG1YRJ0AXPN2QMURADJLUGFPHOS"
+                                 callbackURL:@"liiffee://foursquare"];
+
+    NSLog(@"access token? %@",[Foursquare2 accessToken]);
+//    [Foursquare2 venueSearchNearByLatitude:@ longitude:<#(NSNumber *)longitude#> query:<#(NSString *)query#> limit:<#(NSNumber *)limit#> intent:<#(FoursquareIntentType)intent#> radius:<#(NSNumber *)radius#> categoryId:<#(NSString *)categoryId#> callback:<#(Foursquare2Callback)callback#>]
+}
+
+- (void)setupInstagram
+{
+    InstagramEngine *sharedEngine = [InstagramEngine sharedEngine];
+
+    // testing
+//    [[InstagramEngine sharedEngine] getPopularMediaWithSuccess:^(NSArray *media) {
+//        NSLog(@"%@", media);
+//    } failure:^(NSError *error) {
+//        NSLog(@"Load Popular Media Failed");
+//    }];
+}
+
+#pragma mark - Handle incoming URLs
+
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [Foursquare2 handleURL:url];
 }
 
 @end

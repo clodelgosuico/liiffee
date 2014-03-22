4bafc546fb0b22c72a0e9ebe17f44cfa9b4ee21a
Agathe Battestini <agathe@storenvy.com>
2014-03-22 09:54:40 -0700

    initial commit

---
+++ IOSLiiffee/IOSLiiffee/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSLiiffee
+//
+//  Created by Agathe Battestini on 3/22/14.
+//  Copyright (c) 2014 Liiffee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOSLiiffee/IOSLiiffee/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOSLiiffee
+//
+//  Created by Agathe Battestini on 3/22/14.
+//  Copyright (c) 2014 Liiffee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
